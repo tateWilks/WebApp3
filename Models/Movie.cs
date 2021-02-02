9f40d4dcf41718f734997b46f585a60c5bc8f7a3
tateWilks <64288630+tateWilks@users.noreply.github.com>
2021-02-02 00:00:57 -0700

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp3.Models
+{
+    public class Movie
+    {
+        [Required(ErrorMessage = "This is a required field")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = "This is a required field")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "This is a required field")]
+        public string Year { get; set; }
+        [Required(ErrorMessage = "This is a required field")]
+        public string Director { get; set; }
+        [Required(ErrorMessage = "This is a required field")]
+        public string Rating { get; set; }
+        [Required(ErrorMessage = "This is a required field")]
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        [MaxLength(25, ErrorMessage = "Maximum number of characters is 25")]
+        public string Notes { get; set; }
+    }
+}
